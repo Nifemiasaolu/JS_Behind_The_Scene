44da73fc5fda78115a5f473605eafdb4c4efc9f1
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-19 15:25:55 +0100

    Scoping in practice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, your age is ${age}, born in ${birthYear}`;
+    // return output
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1995) {
+      var millenial = true;
+      const str = `Oh! You are a millenial, ${firstName}`;
+      console.log(str);
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
