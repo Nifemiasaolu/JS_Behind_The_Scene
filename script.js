6c4c49729eec3a4a7972fa1e52da1d707721722b
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-22 13:15:11 +0100

    Hoisting and TDZ Practice

--- script.js
+++ script.js
@@ -6,26 +6,27 @@ function calcAge(birthYear) {
   function printAge() {
     let output = `${firstName}, your age is ${age}, born in ${birthYear}`;
     // return output
-    console.log(output);
+    // console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1995) {
       var millenial = true;
     //   Creating NEW variable with same name with as outer scopes 
       const lastName = 'Steven'
-
+ 
     //   Reassigning outer scope's variable 
-      output = 'NEW OUTPUT'
+      // output = 'NEW OUTPUT'
 
-      const str = `Oh! You are a millenial, ${firstName}`;
-      console.log(str);
+
+      // const str = `Oh! You are a millenial, ${firstName}`;
+      // console.log(str);
 
       function add(a, b) {
         return a + b;
       }
-      console.log(add(2, 3));
+      // console.log(add(2, 3));
     }
-    console.log(millenial);
-    console.log(output);
+    // console.log(millenial);
+    // console.log(output);
     
 }
   printAge();
@@ -34,3 +35,17 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+
+////////////////////////////////////
+// Hoisting and TDZ in Practice 
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+
+var me = 'Jonas';
+let job = 'Teacher';
+// const year = 1991;
+// 
+
